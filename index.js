9735b81b4cfd3303a2557f257991bb86861cd0ae
thanhtacong1995 <thanhtacong1995@gmail.com>
2020-02-16 12:04:21 +0700

    Homework

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
+// 3a
 // for (let i = 0; i < 8; i++){
 //     console.log(i)
 // }
-
+// 3b
 // let n = Number(prompt('Enter n'))
 // for(let i = 0; i < n; i++){
 //     console.log(i)
 // }
-
+// 3c
 // let n = Number(prompt('Enter n'))
 // for(let i = 3; i<n; i++){
 //     console.log(i)
 // }
-
+// 3d
 // let c = Number(prompt('Enter c'))
 // let n = Number(prompt('Enter n'))
 // for(let i = c; i<n; i++){
 //    console.log(i)
 //  }
-
+// 3e
+// let c = Number(prompt('Enter c'))
+// let n = Number(prompt('Enter n'))
+// for(let i = c; i<n; i++=3){
+//    console.log(i)
+//  }
+// 3f
 // let c = Number(prompt('Enter c'))
 // let n = Number(prompt('Enter n'))
 // let s = Number(prompt('Enter s'))
 // for(let i = c; i<n; i+=s){
 //    console.log(i)
 //  }
-
-// let a = Number(prompt())
+// 4
+// let a = Number(prompt(Enter number))
 // let b = 1
 // for(let i=a; i>=1; i--){
 //  b = b*i
 // }
 // console.log (b)
-
+// 5
 // let dob = Number(prompt('Enter date of birth'))
 // if (dob>14){
 //     console.log('Ok')
@@ -39,7 +46,7 @@
 // else{
 //     console.log('you are not old enough')
 // }
-
+// 6
 // let a = Number(prompt('Enter number'))
 // if(a>4.5){
 //     console.log('Higher half of 9')
@@ -47,51 +54,98 @@
 // else{
 //     console.log('Lower half of 9')
 // }
-
-
+// 7
+// let a = Number(prompt('Enter number a'))
+// let b = Number(prompt('Enter number b'))
+// if(a>b){
+//     console.log(`${a} is higher half of ${b}`)
+// }
+// else{
+//     console.log(`${a} is lower half of ${b}`)
+// }
+// 8
+// let a = Number(prompt('Enter number'))
+// if( a%2==0){
+//     console.log(`${a} is a even number`)
+// }
+// else{
+//     console.log(`${a} is a odd number`)
+// }
+// 9a
 // for(let i=0; i<3; i++) {
 //     console.log('H')
 //     }
 //     for(let i=0; i<3; i++) {
 //         console.log('L')
 //         }
-
-// let a = Number(prompt())
+// 9b
+// let a = Number(prompt('Enter total number of H and L'))
 // for(let i=0; i<Math.floor(a/2); i++) {
 //         console.log('H')
 //          }
 //          for(let i=0; i<a/2; i++) {
 //              console.log('L')
 //              }
-
+// 9c
 // for(let i=0; i<4; i++) {
 //     console.log(i-i+1)
 //     console.log(i-i)
 // }
-
+// 9d
 // let a = Number(prompt())
 // for(let i=0; i<a/2; i++) {
 //     console.log(i-i+1)
 //     console.log(i-i)
 // }
-
-let mass = Number(prompt())
-let height = Number(prompt())
-let trueheight = height/100
-let BM = mass/(trueheight**2)
-console.log(' your BMI is ',BM)
-if(BM<16){
-    console.log('severly underweight')
-} 
-else if (16<BM<18.5){
-    console.log('underweight')
-}
-else if (18.5<BM<25){
-    console.log('normal')
-}
-else if (25<BM<30){
-    console.log('overweight')
-}
-else{
-    console.log('obese')
-}
+// 10
+// let mass = Number(prompt('Enter weight in kg'))
+// let height = Number(prompt('Enter height in cm'))
+// let trueheight = height/100
+// let BM = mass/(trueheight**2)
+// console.log(' your BMI is ',BM)
+// if(BM<=16){
+//     console.log('severly underweight')
+// } 
+//  else if (16<=BM<=18.5){
+//     console.log('underweight')
+// }
+//  else if (18.5<=BM<=25){
+//     console.log('normal')
+// }
+//  else if (25<=BM<=30){
+//     console.log('overweight')
+// }
+// else {
+//     console.log('obese')
+// }
+// 11a
+// clear()
+// for(let i=0; i<4; i++){
+//     fd(100)
+//     lt(90)
+// }
+// 11b
+// clear()
+// for(let i=0; i<3; i++){
+//     fd(100)
+//     lt(120)
+// }
+// 11c
+// clear()
+// for(let i=0; i<5; i++){
+//     fd(100)
+//     lt(72)
+// }
+// 11d
+// clear()
+// for(let i=0; i<6; i++){
+//     fd(100)
+//     lt(60)
+// }
+// 12
+// clear()
+// let n = Number(prompt('Enter number of edges'))
+// for(let i=0; i<n; i++){
+//     fd(100)
+//     lt(360/n)
+// }
